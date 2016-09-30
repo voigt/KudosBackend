e68846ab76585db0957bd96331f43e32e98a43ce
Christoph Voigt <voigt.christoph@gmail.com>
2016-10-01 00:26:19 +0200

    add test files

---
+++ kudos_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSelectMode(t *testing.T) {
+	/*
+		cases := []struct {
+			args    []string,
+		}{
+			{["sqlite", "Hello this is a note"]},
+			{["sqlite", "view"]},
+			{["sqlite", "view", "1"]},
+			{["sqlite", "reset"]},
+
+		}
+	*/
+
+	t.Error("bla")
+}
---
+++ models/handler_test.go
@@ -0,0 +1,29 @@
+package models
+
+import (
+	"testing"
+)
+
+func TestGetAllKudos(t *testing.T) {
+	/*
+		cases := []struct {
+			args    []string,
+		}{
+			{["sqlite", "Hello this is a note"]},
+			{["sqlite", "view"]},
+			{["sqlite", "view", "1"]},
+			{["sqlite", "reset"]},
+
+		}
+	*/
+
+	t.Error("bla")
+}
+
+func TestGetKudos(t *testing.T) {
+
+}
+
+func TestPostKudos(t *testing.T) {
+
+}
