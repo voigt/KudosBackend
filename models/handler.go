03d3c3321c3dab235b03c180f1491fe161d88858
Christoph Voigt <voigt.christoph@gmail.com>
2016-10-04 21:18:45 +0200

    every handler now responds with valid json added proper logging

--- models/handler.go
+++ models/handler.go
@@ -1,118 +1,119 @@
 package models
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
-func Default(w http.ResponseWriter, r *http.Request) {
-
-	w.Write([]byte("Nothing here :)"))
-
+type Website struct {
+	Id    int    `json:"id"`
+	Url   string `json:"url"`
+	Kudos int    `json:"kudos"`
 }
 
-func GetAllKudos() {
+type Websites []Website
+
+func GetAllKudos(w http.ResponseWriter, r *http.Request) {
+
+	var website Website
+	var websites Websites
 
 	rows, err := db.Query("SELECT id, url, kudos FROM kudos")
 	checkErr(err)
 
 	for rows.Next() {
-		var id int
-		var url string
-		var kudos int
-		err = rows.Scan(&id, &url, &kudos)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(id, url, kudos)
+		err = rows.Scan(&website.Id, &website.Url, &website.Kudos)
+		checkErr(err)
+
+		websites = append(websites, website)
+
 	}
+	json.NewEncoder(w).Encode(websites)
+
 	err = rows.Err()
 	checkErr(err)
 
 }
 
-func GetKudoCount(url string) int {
+func GetKudoCount(url string) Website {
 
 	rows, err := db.Query("SELECT * FROM kudos WHERE kudos.url = (?)", url)
 	checkErr(err)
 
-	type Result struct {
-		ID    int
-		URL   string
-		Kudos int
-	}
-
-	var result Result
+	var website Website
 
 	for rows.Next() {
-		err = rows.Scan(&result.ID, &result.URL, &result.Kudos)
+		err = rows.Scan(&website.Id, &website.Url, &website.Kudos)
 		checkErr(err)
 	}
 
-	log.Printf("Requested URL %s (%d Kudos)", url, result.Kudos)
+	log.Printf("Query URL %s for Kudos... %d Kudos.", url, website.Kudos)
 
-	return result.Kudos
+	return website
 
 }
 
-func PostKudoCount(url string) int {
+func PostKudoCount(url string) Website {
 
-	doesUrlExist := GetKudoCount(url)
+	website := GetKudoCount(url)
 
-	var id int
-
-	if doesUrlExist != 0 {
+	if website.Id != 0 {
 		log.Printf("%s is already known.", url)
-		stmt, err := db.Prepare("UPDATE kudos set kudos = kudos + 1 WHERE kudos.url = (?)")
+		_, err := db.Exec("UPDATE kudos set kudos = kudos + 1 WHERE kudos.url = (?)", url)
+		//stmt, err := db.Prepare("UPDATE kudos set kudos = kudos + 1 WHERE kudos.url = (?)")
 		checkErr(err)
 
-		res, err := stmt.Exec(url)
-		checkErr(err)
+		website := GetKudoCount(url)
 
-		id, err := res.LastInsertId()
-		checkErr(err)
+		log.Printf("Kudos for URL: %s with ID #%d given.", website.Url, website.Id)
 
-		log.Printf("Kudos for URL: %s with ID #%d given.", url, id)
+		return website
 	} else {
-		stmt, err := db.Prepare("INSERT INTO kudos(url, kudos) values (?, 1)")
+		website.Url = url
+		website.Kudos = 1
+
+		stmt, err := db.Prepare("INSERT INTO kudos(url, kudos) values (?, ?)")
 		checkErr(err)
 
-		res, err := stmt.Exec(url)
+		res, err := stmt.Exec(website.Url, website.Kudos)
 		checkErr(err)
 
 		id, err := res.LastInsertId()
 		checkErr(err)
+		website.Id = int(id)
 
-		log.Printf("Created URL: %s with ID #%d", url, id)
+		log.Printf("Created URL: %s with ID #%d", url, website.Id)
+		return website
 	}
-
-	return id
 
 }
 
 func GetKudos(w http.ResponseWriter, r *http.Request) {
 
+	var website Website
+
 	params := mux.Vars(r)
 	url := params["url"]
 
-	kudos := strconv.Itoa(GetKudoCount(url))
+	website = GetKudoCount(url)
 
-	log.Printf("Requested URL %s (%d Kudos)", url, kudos)
-	w.Write([]byte(kudos))
+	json.NewEncoder(w).Encode(website)
 
 }
 
 func PostKudos(w http.ResponseWriter, r *http.Request) {
 
+	var website Website
+
 	params := mux.Vars(r)
-	url := params["url"]
+	website.Url = params["url"]
 
-	id := strconv.Itoa(PostKudoCount(url))
+	website = PostKudoCount(website.Url)
 
-	w.Write([]byte("Kudos for URL: " + url + " with ID #" + id + " given."))
+	//w.Write([]byte("Kudos for URL: " + website.Url + " with ID #" + id + " given."))
+	json.NewEncoder(w).Encode(website)
 
 }
