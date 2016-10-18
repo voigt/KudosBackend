71cbbd4848b7bf4092c98f9b6d16a41369c478c0
Christoph Voigt <voigt.christoph@gmail.com>
2016-10-19 00:05:38 +0200

    refactor model.db

--- models/db.go
+++ models/db.go
@@ -2,39 +2,35 @@ package models
 
 import (
 	"database/sql"
-	"log"
 	"os"
 )
 
 var db *sql.DB
 
 func InitDB(dataSourceName string) {
-	var err error
-	db, err = sql.Open("sqlite3", dataSourceName)
-	if err != nil {
-		log.Panic(err)
-	}
+	db, err := sql.Open("sqlite3", dataSourceName)
+	checkErr(err)
 
-	// Ping verifies a connection to the database is still alive,
-	// establishing a connection if necessary.
-	if err = db.Ping(); err != nil {
-		log.Panic(err)
+	if db == nil {
+		panic("db nil")
 	}
+}
+
+func PopulateDB() {
+	sql := `
+		CREATE TABLE IF NOT EXISTS kudos (
+			id integer not null primary key autoincrement,
+			url text,
+			kudos integer
+		);
+	`
+
+	_, err := db.Exec(sql)
+	checkErr(err)
 }
 
 func ResetDB() {
 	// Remove old DB
 	os.Remove("./kudos.db")
 	PopulateDB()
-}
-
-func PopulateDB() {
-
-	// creates table
-	sqlStmt := `
-	create table kudos (id integer not null primary key autoincrement, url text, kudos integer);
-	`
-
-	_, err := db.Exec(sqlStmt)
-	checkErr(err)
 }
